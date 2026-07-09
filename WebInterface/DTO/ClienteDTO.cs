c2f8d958fad4032d3a4f10df8171b7fb0c705ed2
frezatti <68041159+frezatti@users.noreply.github.com>
2026-07-09 00:33:50 -0300

    Align frontend client DTO with proxy JSON

--- WebInterface/DTO/ClienteDTO.cs
+++ WebInterface/DTO/ClienteDTO.cs
@@ -2,9 +2,8 @@
 
 public class ClienteDto
 {
-    public int? Id { get; set; }
-    public string Codigo { get; set; } = "";
-    public string Nome { get; set; } = "";
-    public string Telefone { get; set; } = "";
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
     public string Email { get; set; } = "";
+    public string Number { get; set; } = "";
 }
